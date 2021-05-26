707a39daaccf4b85af1ab2cb0da920d4e25bab13
Max <tomilovediana@gmail.com>
2021-05-26 16:13:29 +0500

    for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi --log-file -
+web: gunicorn anfisa4friends.wsgi --log-file -
--- anfisa4friends/settings.py
+++ anfisa4friends/settings.py
@@ -67,6 +67,6 @@ LANGUAGE_CODE = 'ru'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
